e59bf047f136da3e36c58e955149cac17a2d0bfc
Ori Ben Amram <benamram.ori@gmail.com>
2022-07-14 11:40:40 +0300

    deleted un-used files

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,19 +1,29 @@
-
 import React, { useEffect, useState } from 'react';
 
 export function LoginPage() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-
-    useEffect(() => { 
-        
-    }, [])
+    const [counter, setCounter] = useState(0)
 
     const onSubmit = (ev) => {
         ev.preventDefault()
         console.log('submit')
         console.log('username:', username);
         console.log('password:', password);
+    }
+
+    const onClickBtn = () => { 
+        console.log('counter:', counter);
+        for(let i = 0; i < 3; i++) { 
+            setCounter(prevState => {
+                return prevState + 1
+            })
+        }
+        console.log('counter:', counter);
+        // setCounter(prevState => {
+        //     console.log('prevState:', prevState);
+        //     return prevState
+        // })
     }
 
     const isDisabled = (username && password) ? false : true
@@ -40,6 +50,7 @@ export function LoginPage() {
                 />
                 <button className={btnClasses} disabled={isDisabled}>Login</button>
             </form>
+            <button onClick={onClickBtn}>Counter</button>
         </div>
     )
 }
--- src/pages/login-page.test.js
+++ src/pages/login-page.test.js
@@ -1,21 +1,16 @@
-import { LoginPage } from './login-page'
+import { LoginPage } from "./login-page";
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 
-
-describe('login - submit btn', () => {
-    test('disabled when fields are empty', () => {
-        render(<LoginPage />);
+describe('LoginPage', () => { 
+    test('disabled when field are empty', () => { 
+        render(<LoginPage />)
         expect(screen.getByRole('button', { name: /Login/i })).toBeDisabled()
     })
-
-    test('ebabled when fields are not empty', () => {
-        render(<LoginPage />);
-        userEvent.type(screen.getByPlaceholderText(/Enter username/i), "ori")
+    test('enabled when field are not empty', () => { 
+        render(<LoginPage />)
+        userEvent.type(screen.getByPlaceholderText(/Enter username/i), "Moshe")
         userEvent.type(screen.getByPlaceholderText(/Enter password/i), "123")
-
         expect(screen.getByRole('button', { name: /Login/i })).toBeEnabled()
-
-
     })
 })
